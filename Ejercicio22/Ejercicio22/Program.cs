b94cb6de09408629f0aaf6b39425f1a8b50dc11d
diego.taub <diego.taub@accenture.com>
2021-03-25 21:20:00 -0300

    Ejercicio22

---
+++ Ejercicio22/Ejercicio22/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"La fecha actual en formato dd/MM/yyyy es: {DateTime.Now.ToString("dd/MM/yyyy")}");
+            Console.ReadKey();
+        }
+    }
+}
